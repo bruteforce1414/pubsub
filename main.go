8401f06d5915ac2ff69ec400dffeee91d4175aa9
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-16 12:27:59 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/bruteforce1414/pubsub/observer"
+
+func main() {
+
+	shirtItem := newItem("Nike")
+
+	observerFirst := &customer{id: "abc@gmail.com"}
+	observerSecond := &customer{id: "xyz@gmail.com"}
+
+	shirtItem.register(observerFirst)
+	shirtItem.register(observerSecond)
+
+	shirtItem.updateAvailability()
+}
