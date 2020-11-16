9baaa3c9df527dbb50fdb89f208afa3dd0a64e57
bruteforce1414 <bruteforce1414@gmail.com>
2020-11-16 17:36:20 +0500

    feat: add TestRegister

--- observer/item_test.go
+++ observer/item_test.go
@@ -18,12 +18,20 @@ func TestNewItem(t *testing.T) {
 		name:         "Shirt Adidas",
 		inStock:      false,
 	}
+
 	testItem1 := NewItem("Shirt Adidas")
 	testItem2 := NewItem("Shirt Reebok")
-
-
 	assert.Equal(t, testItem1.name, itemForCompare1.name)
 	assert.NotEqual(t, testItem2.name, itemForCompare1.name)
+}
 
+func TestRegister(t *testing.T){
+	testItem1 := NewItem("Nike shirt")
+	observerTest1 := &Customer{Id: "abc@gmail.com"}
+	observerTest2 := &Customer{Id: "xyz@gmail.com"}
+	testItem1.Register(observerTest1)
+	testItem1.Register(observerTest2)
 
+	assert.Equal(t,testItem1.observerList[0].getID(), "abc@gmail.com");
+	assert.Equal(t,testItem1.observerList[1].getID(), "xyz@gmail.com");
 }
